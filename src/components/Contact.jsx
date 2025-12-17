0ea4ea6f1582301abd566dda899ac5368267fa39
eqan-gg <eqaanchauhan@gmail.com>
2025-12-17 16:18:57 +0500

    fixing folder strcuture

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,7 +16,7 @@ const Contact = () => {
     setEmail('');
     setMessage('');
 
-    const response = await fetch("http://localhost:3000/api/contact", {
+    const response = await fetch("https://portfolio-server-vev8.onrender.com/api/contact", {
       method : "POST",
       headers : {
         "Content-Type" : "application/json"
